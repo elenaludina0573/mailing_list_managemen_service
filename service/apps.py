3a6728ff93db746aafbee67b90445a603296023e
elenaludina0573 <elenaludina0@gmail.com>
2024-06-04 20:33:51 +0300

    Change

--- service/apps.py
+++ service/apps.py
@@ -5,7 +5,3 @@ class ServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'service'
     verbose_name = 'Сервис'
-
-    def ready(self):
-        from service.scheduler import start
-        start()
